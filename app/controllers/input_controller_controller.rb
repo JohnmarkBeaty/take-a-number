52e632573f7056db6f50bee57bf3a755afda2a9e
Caleb Gulliver <calebgulliver@gmail.com>
2015-05-11 17:12:34 -0400

    add Twillo

--- app/controllers/input_controller_controller.rb
+++ app/controllers/input_controller_controller.rb
@@ -1,7 +1,19 @@
 class InputControllerController < ApplicationController
   def create
+    # render plain: params.inspect
     @person = Queuedperson.new(queuedperson_params)
     @person.save
+    twilio_sid = ENV['twilio_sid']
+    twilio_token = ENV['twilio_token']
+    twilio_phone_number = ENV['twilio_phone_number']
+
+    @twillo_clinet = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twillo_clinet.account.sms.messages.create(
+    from: "+1#{twilio_phone_number}",
+    to: @person.phonenumber,
+    body: "Congradulations your amazing!"
+    )
   end
 
   def show
@@ -9,6 +21,6 @@ class InputControllerController < ApplicationController
   end
 
   def queuedperson_params
-    params.require(:queuedperson).permit(:firstname, :lastname, :phonenumber)
+    params.require(:person).permit(:firstname, :lastname, :phonenumber)
   end
 end
